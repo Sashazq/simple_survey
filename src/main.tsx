57601db5edcf7fba164eb53d5388531367e242a6
Oleksandr Bidenko <sashazq@gmail.com>
2024-06-06 16:57:01 +0200

    Deploy update, react router fix for relative path

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,17 @@ import App from "./App.tsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+    },
+  ],
   {
-    path: "/",
-    element: <App />,
-  },
-]);
+    basename: "/simple_survey",
+  }
+);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
